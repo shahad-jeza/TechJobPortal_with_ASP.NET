3848dcc7222ecb8475921b6cb68fc91cdf71b831
shahad_jeza <shoodialamri@gmail.com>
2025-02-27 14:33:12 +0300

    added sort based on date functionallity

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -10,18 +10,29 @@ namespace TechJobPortal.Controllers
     {
         private static List<JobListing> jobListings = new List<JobListing>
         {
-            new JobListing { Id = 1, Title = "Software Engineer", CompanyName = "ELM", Location = "Ryiadh", JobType = JobType.FullTime, PostedDate = DateTime.Now },
-            new JobListing { Id = 2, Title = "Data Analyst", CompanyName = "SDAIA", Location = "Neom", JobType = JobType.Remote, PostedDate = DateTime.Now },
-            new JobListing { Id = 1, Title = "senior dev", CompanyName = "SCAI", Location = "Jeddah", JobType = JobType.FullTime, PostedDate = DateTime.Now },
-            new JobListing { Id = 2, Title = "Quality assurance engnieer", CompanyName = "san hospital", Location = "San Francisco", JobType = JobType.Remote, PostedDate = DateTime.Now },
+            new JobListing { Id = 1, Title = "Software Engineer", CompanyName = "ELM", Location = "Ryiadh", JobType = JobType.FullTime, PostedDate =new  DateTime(2025, 1, 10)},
+            new JobListing { Id = 2, Title = "Data Analyst", CompanyName = "SDAIA", Location = "Neom", JobType = JobType.Remote, PostedDate = new DateTime(2025, 2, 19) },
+            new JobListing { Id = 1, Title = "senior dev", CompanyName = "SCAI", Location = "Jeddah", JobType = JobType.FullTime, PostedDate = new DateTime(2025, 1, 10)},
+            new JobListing { Id = 2, Title = "Quality assurance engnieer", CompanyName = "san hospital", Location = "San Francisco", JobType = JobType.Remote, PostedDate = new  DateTime(2025, 1, 5)},
             new JobListing { Id = 1, Title = "Mobil develper", CompanyName = "ELM", Location = "Thawl", JobType = JobType.FullTime, PostedDate = DateTime.Now },
             new JobListing { Id = 2, Title = "web developer", CompanyName = "Data Inc.", Location = "San Francisco", JobType = JobType.Remote, PostedDate = DateTime.Now }
         };
+    public IActionResult Index(string sortOrder)
+    {
+        var sortedJobs = jobListings.AsQueryable();  // Start with all jobs
 
-        public IActionResult Index()
+        // Check sort order and apply sorting
+        if (sortOrder == "newest")
         {
-            return View(jobListings);
+            sortedJobs = sortedJobs.OrderByDescending(j => j.PostedDate);
         }
+        else if (sortOrder == "oldest")
+        {
+            sortedJobs = sortedJobs.OrderBy(j => j.PostedDate);
+        }
+
+        return View(sortedJobs.ToList());  // Ensure sorted list is passed to the view
+    }
 
         public IActionResult Details(int id)
         {
@@ -32,5 +43,39 @@ namespace TechJobPortal.Controllers
             }
             return View(job);
         }
+
+
+    // add edit logic for task 9 
+        public IActionResult Edit(int id)
+    {
+        var job = jobListings.FirstOrDefault(j => j.Id == id);
+        if (job == null)
+        {
+            return NotFound();
+        }
+
+        return View(job);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, JobListing updatedJob)
+    {
+        var job = jobListings.FirstOrDefault(j => j.Id == id);
+        if (job == null)
+        {
+            return NotFound();
+        }
+
+        // Update job details
+        job.Title = updatedJob.Title;
+        job.CompanyName = updatedJob.CompanyName;
+        job.Location = updatedJob.Location;
+        job.JobType = updatedJob.JobType;
+        job.PostedDate = updatedJob.PostedDate;
+
+        return RedirectToAction("Index");
+    }
+
+        
     }
 }
--- obj/Debug/net9.0/TechJobPortal.AssemblyInfo.cs
+++ obj/Debug/net9.0/TechJobPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechJobPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed6b8f837396ef15a5eef0908c2581bd5eb828d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechJobPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechJobPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
